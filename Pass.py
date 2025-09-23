bb3aff32890db7c82e27ab166ac44063dc2e8679
AdhvikV21 <adzydarock@gmail.com>
2025-09-23 18:53:57 +1000

    Add files via upload

---
+++ Pass.py
@@ -0,0 +1,15 @@
+for x in range(20): #iterate for loop
+    if x % 20 == 0: #condition 1
+        print("twist")
+
+    elif x % 15 == 0: #condition 2
+        pass
+
+    elif x % 5 == 0: #condition 3
+        print("fizz") 
+    
+    elif x % 3 == 0: #conditon 4
+        print("buzz")
+    
+    else: #conditon 5
+        print (x)
